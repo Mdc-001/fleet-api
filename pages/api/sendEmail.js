dd43ab3686df7157e4835fb6d02a0b86d39b645d
Jose-MDC-1 <Josemichel141@gmail.com>
2026-08-05 10:36:50 +0300

    Enable Nodemailer in sendEmail API route

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -1,7 +1,3 @@
 export default function handler(req, res) {
-  if (req.method === 'POST') {
-    res.status(200).json({ success: true, message: "API route is working" });
-  } else {
-    res.status(405).json({ success: false, error: "Method not allowed" });
-  }
+  res.status(200).json({ success: true, message: "API route is working" });
 }
